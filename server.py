0ffebd8eaff9854cdc1a4c1517b78268e3705c65
Zarif Atique <zarifatique02@gmail.com>
2024-12-10 20:54:39 +1100

    draft for server.py

--- server.py
+++ server.py
@@ -1,8 +1,33 @@
 import socket
+import threading
 
-sserver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sserver.bind(("172.26.91.229", 10000)) # uses port 10000
+PORT = 10000
+SERVER = socket.gethostbyname(socket.gethostname())
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
-while True:
-    data, address = server.recvfrom(256)
-    print ("Message from %s: %s" % (str(address), data))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", PORT)) # Listen on all available network interfaces
+
+def handle_client(conn, addr):
+    print(f"Connected to {addr}.")
+    connected = True;
+    while connected:
+        msg = conn.recv(64).decode('utf-8')     # Receive 64 bytes of data from the client
+        print(f"Received from client {addr}: {msg}")
+        if msg == DISCONNECT_MESSAGE:
+            print(f"Client {addr} has disconnected.")
+            break
+    conn.close()
+
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        # thread = threading.Thread(target=handle_client, args(conn, addr))
+        # thread.start()
+        handle_client(conn, addr)
+        
+
+
+start()
