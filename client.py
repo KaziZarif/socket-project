952f6251d6dec1029ee7fd7aad9244a50d1a1066
Zarif Atique <zarifatique02@gmail.com>
2024-12-11 18:01:26 +1100

    client basic completed

--- client.py
+++ client.py
@@ -1,4 +1,30 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client.sendto(b"Hello world!", ("172.26.91.229", 10000)) # port 10000
+PORT = 10000
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = socket.gethostbyname(socket.gethostname())
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def start():
+    client.connect((SERVER, PORT))
+    print(f"Connect to server at {SERVER}:{PORT}")
+    try:
+        while True:
+            try:
+                msg = input("Send: ")
+            except EOFError:
+                print("Diconnecting....")
+                break
+
+            client.send(msg.encode('utf-8'))
+            
+            if msg.upper() == DISCONNECT_MESSAGE:
+                print("Disconnect message sent. Closing connection....")
+                break 
+    finally:
+        client.close()
+        print("Disconnected from server.")
+
+start()
